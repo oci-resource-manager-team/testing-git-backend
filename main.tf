31981c1dad72b2530b3876a12b66669e1e57232c
oci-resource-manager-team <59630159+oci-resource-manager-team@users.noreply.github.com>
2020-01-07 17:28:34 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ variable "compartment_ocid" {}
 variable "region" {}
 variable "display_name_prefix" {}
 
-resource "null_resource" "basic" {
+resource "null_resource" "git_backend" {
   provisioner "local-exec" {
     command = "echo \"testing git backend from Github private repo\""
   }
